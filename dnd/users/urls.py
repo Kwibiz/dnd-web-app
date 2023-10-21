bc2caf364eb6b0b57e6c00791b8f73303bfcb46c
Viktor Malovanyi <quibiz.ua@gmail.com>
2023-10-21 13:02:28 +0300

    added login function

--- dnd/users/urls.py
+++ dnd/users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', views.LoginUserView.as_view(), name='login'),
+    path('main/', views.users_main, name='users_main')
 ]
--- dnd/users/views.py
+++ dnd/users/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout 
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
 
+@login_required
+def users_main(request):
+    user = request.user.username
+    return render(request, 'users/users_main.html', context={'user': user})
 
-def login(request):
-    return render(request, 'users/login.html')
+
+def logout_view(request):
+    logout(request)
+
+
+class LoginUserView(LoginView):
+    template_name = 'users/login.html'
+    next_page = 'users_main'
+    redirect_authenticated_user = True
