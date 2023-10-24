0dd5bcfff7ff92df45c19db00b7245cfde43dcfd
Viktor Malovanyi <quibiz.ua@gmail.com>
2023-10-24 11:40:08 +0300

    added email field to UserProfile model

--- dnd/users/models.py
+++ dnd/users/models.py
@@ -5,3 +5,4 @@ class UserProfile(models.Model):
     first_name = models.CharField(max_length=20, verbose_name='First Name')
     last_name = models.CharField(max_length=20, verbose_name='Last Name')
     role = models.CharField(choices=[('DM', 'Dungeon Master'), ('Player', 'Player')], max_length=30, verbose_name='Role')
+    email = models.EmailField(null=True, verbose_name='Email')
