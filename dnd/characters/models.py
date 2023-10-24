72664fc83d4c6ca850a5b98a5023d1254f959028
Viktor Malovanyi <quibiz.ua@gmail.com>
2023-10-24 11:47:39 +0300

    Character model refactoring

--- dnd/characters/models.py
+++ dnd/characters/models.py
@@ -2,14 +2,29 @@ from django.db import models
 
 
 class Character(models.Model):
+    RACE_CHOICES = (
+        ("Dragonborn", "Dragonborn"),
+        ("Dwarf", "Dwarf"),
+        ("Elf", "Elf"),
+        ("Gnome", "Gnome"),
+        ("Half Elf", "Half Elf"),
+        ("Half Orc", "Half Orc"),
+        ("Halfling", "Halfling"),
+        ("Human", "Human"),
+        ("Tiefling", "Tiefling")
+    )
+
     name = models.CharField(max_length=50, verbose_name='Character Name')
     level = models.IntegerField(default=1, verbose_name='Level')
-    experience_points = models.IntegerField(default=0, verbose_name='Experience Points')
-    character_class = models.CharField(max_length=20, verbose_name='Class')
-    background = models.TextField(blank=True, null=True, verbose_name='Background')
-    race = models.CharField(choices=[('Dragonborn', 'Dragonborn'), ('Dwarf', 'Dwarf'), ('Elf', 'Elf'), ('Gnome', 'Gnome'), ('Half Elf', 'Half Elf'), ('Half Orc', 'Half Orc'), ('Halfling', 'Halfling'), ('Human', 'Human'), ('Tiefling', 'Tiefling')], max_length=30, verbose_name='Race')
-    alignment = models.CharField(max_length=70, verbose_name='Alignment')
-    player_name = models.CharField(max_length=50, verbose_name="Player's name")
+    character_class = models.CharField(max_length=50, verbose_name='Class')
+    background = models.TextField(verbose_name='Background', null=True, blank=True)
+    player_name = models.CharField(max_length=50, verbose_name='Players Name')
+    race = models.CharField(choices=RACE_CHOICES, max_length=20, verbose_name='Race')
+    alignment = models.CharField(max_length=50, verbose_name='Alignment')
+    experience_points = models.PositiveIntegerField(default=0, verbose_name='Experience Points')
+
+    def __str__(self):
+        return self.name.capitalize()
 
     class Meta:
         constraints = [
