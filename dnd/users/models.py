9124a8af0b28dd7865e1113a3ce1a6b02fa7069e
Viktor Malovanyi <quibiz.ua@gmail.com>
2023-10-24 11:24:46 +0300

    added UserProfile model

--- dnd/users/models.py
+++ dnd/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=20, verbose_name='First Name')
+    last_name = models.CharField(max_length=20, verbose_name='Last Name')
+    role = models.CharField(choices=[('DM', 'Dungeon Master'), ('Player', 'Player')], max_length=30, verbose_name='Role')
