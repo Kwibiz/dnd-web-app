016362091777a4f4e56366b9ff25062b1353fc0f
Viktor Malovanyi <quibiz.ua@gmail.com>
2023-10-24 13:24:03 +0300

    added redirect from main page

--- dnd/dnd/urls.py
+++ dnd/dnd/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(pattern_name='users:users_main', permanent=True)),
     path('users/', include('users.urls')),
     path('characters/', include('characters.urls')),
 ]
